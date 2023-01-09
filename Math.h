8424ed040569120c6046c40d3cc237ff11cfe213
Aleksthor <athorset98@gmail.com>
2023-01-09 14:17:14 +0100

    Initial commit

---
+++ Math.h
@@ -0,0 +1,15 @@
+
+
+class Position
+{
+public:
+    Position(int _x, int _y, int _z)
+        :   x(_x), y(_y), z(_z)
+    {
+
+    }
+
+    int x;
+    int y;
+    int z;
+};
---
+++ Vertex0.h
@@ -0,0 +1,12 @@
+#ifndef VERTEX_H
+#define VERTEX_H
+
+struct Vertex0 {
+    float x;
+    float y;
+    float z;
+    float r;
+    float g;
+    float b;
+ };
+ #endif // VERTEX_H
